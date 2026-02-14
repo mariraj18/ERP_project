01c3337b759a5f23ff48f4c1ddee2b6942d7f7ba
mariraj18 <marimari564500@gmail.com>
2026-02-14 12:07:40 +0530

    vercel

--- server/server.js
+++ server/server.js
@@ -28,7 +28,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://erp-project-black.vercel.app/"
+  ],
+  credentials: true
+}));
+
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
